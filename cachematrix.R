551af7ab0ef9052fbf12d47449279e28ea5174df
MooazHammam <moo3az@hotmail.com>
2015-02-21 13:51:23 +0200

    R Course Programming Assignment 2
    my assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## this is a programming assignment 2 on how to Cache large Matrics
 
 ## Write a short comment describing this function
-
+## Create A matrix X and Cache its inverse later
 makeCacheMatrix <- function(x = matrix()) {
 
+        inverse <- NULL
+        set <- function(x) {
+                mtx <<- x;
+                inverse <<- NULL;
+        }
+        get <- function() return(mtx);
+        setinv <- function(inv) inverse <<- inv;
+        getinv <- function() return(inverse);
+        return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 }
 
 
 ## Write a short comment describing this function
-
+## Return matrix that is the inverse of 'x'
+## From Cache if exists or create it and then cache the Matrix and return it
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- mtx$getinv()
+        if(!is.null(inverse)) {
+                message("Getting cached data...")
+                return(inverse)
+        }
+        data <- mtx$get()
+        invserse <- solve(data, ...)
+        mtx$setinv(inverse)
+        return(inverse)
 }
